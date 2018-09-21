d01623199ab6811ea879b3e72027daf35a7a5539
Travis Desir <travisgdesir31@gmail.com>
2018-09-21 02:37:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
      lowest_value = nil 
-     lowest_ key = nil 
+     lowest_key = nil 
      name_hash.collect do |name, value|
        if lowest_value == nil || value < lowest_value
          lowest_value = value
